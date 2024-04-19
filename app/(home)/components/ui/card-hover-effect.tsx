091826bcfd4d019b4fed831f325ae1f9106c9a96
Saurabh Shukla <saurabh1258.be21@chitkarauniversity.edu.in>
2024-04-19 17:56:49 +0530

    responsive-added

--- app/(home)/components/ui/card-hover-effect.tsx
+++ app/(home)/components/ui/card-hover-effect.tsx
@@ -19,7 +19,7 @@ export const HoverEffect = ({
   return (
     <div
       className={cn(
-        "grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  py-10",
+        "grid grid-cols-2  lg:grid-cols-3  py-10",
         className
       )}
     >
@@ -52,7 +52,7 @@ export const HoverEffect = ({
           <div className="rounded-md w-full p-4 overflow-hidden bg-black group-hover:ring-2 ring-green-500 relative z-20 transition-all duration-500 cursor-pointer">
             <div className="py-10 z-50 relative space-y-5">
                 <Icon className="w-8 h-8 mx-auto"/>
-              <p className="text-2xl font-bold text-center text-gray-300 ">{item.title}</p>
+              <p className="text-xl md:text-2xl font-bold text-center text-gray-300 ">{item.title}</p>
             </div>
           </div>
         </div>
